a11b3aebccf0719062e48ad26a855395ce735871
Rin Takahashi <c0b2208944@edu.teu.ac.jp>
2023-10-03 14:47:30 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,12 @@ import random
 
 
 WIDTH, HEIGHT = 1600, 900
-
+delta = {
+    pg.K_UP: (0,-5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +17,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)
     bd_img = pg.Surface((20, 20))
     pg.draw.circle(bd_img, (255, 0, 0),(10, 10), 10)
     bd_img.set_colorkey((0, 0, 0))
@@ -30,10 +37,18 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-
+        """こうかとん"""
+        key_list = pg.key.get_pressed()
+        sum_move = [0, 0]
+        for key, mv in delta.items():
+            if key_list[key]:
+                sum_move[0] += mv[0]
+                sum_move[1] += mv[1]
+        kk_rct.move_ip(sum_move[0], sum_move[1])
+        screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
 
