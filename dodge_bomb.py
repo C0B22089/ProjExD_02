ca1f453204f7d629a48e9492bc04381a4b46292d
Rin Takahashi <c0b2208944@edu.teu.ac.jp>
2023-10-03 14:26:02 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -18,6 +18,9 @@ def main():
     bd_rct = bd_img.get_rect()
     x, y = random.randint(0,WIDTH), random.randint(0, HEIGHT)
     bd_rct.center = (x, y)
+    vx, vy = 0, 0
+    vx += 5
+    vy += 5
 
 
 
@@ -30,10 +33,13 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+
+        bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
+
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
